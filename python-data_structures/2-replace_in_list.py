f1c2b5e550771de4488f3d8163ff05aa16370f9f
lebomofube1818-sys <lebomofube1818-sys@gmail.com>
2025-12-17 13:40:32 +0200

    2. Replace element in a list - complete task

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    """Replace an element at a specific position in a list.
+
+    If idx is negative or out of range, return the original list unchanged.
+    """
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    my_list[idx] = element
+    return my_list
