39b3ae53cb90b6e2ebb0d889c4229cc0a29a3e22
lebomofube1818-sys <lebomofube1818-sys@gmail.com>
2026-01-13 13:40:30 +0200

    Task 1: My first toolbox

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,19 +1,11 @@
 #!/usr/bin/python3
-def add(a, b):
-    """My addition function"""
-    return (a + b)
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
 
+    a = 10
+    b = 5
 
-def sub(a, b):
-    """My subtraction function"""
-    return (a - b)
-
-
-def mul(a, b):
-    """My multiplication function"""
-    return (a * b)
-
-
-def div(a, b):
-    """My division function"""
-    return int(a / b)
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
