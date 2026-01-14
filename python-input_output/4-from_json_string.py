ad38190dafe14ef87ac746d7081bbf412cf89deb
lebomofube1818-sys <lebomofube1818-sys@gmail.com>
2026-01-14 16:57:05 +0200

    Task 4: From JSON string to object

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module that converts a JSON string to a Python object."""
+import json
+
+
+def from_json_string(my_str):
+    """Returns a Python object represented by a JSON string.
+
+    Args:
+        my_str (str): JSON string to convert
+
+    Returns:
+        The Python data structure represented by the JSON string
+    """
+    return json.loads(my_str)
