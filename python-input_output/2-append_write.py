f6c7c47ec5011f01540aca6ba7ab73987cb6601a
lebomofube1818-sys <lebomofube1818-sys@gmail.com>
2026-01-14 16:47:36 +0200

    Fix PEP8: add 2 blank lines before function

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module that provides a function to append a string to a text file."""
 
+
 def append_write(filename="", text=""):
     """Appends a string to the end of a UTF8 text file.
 
