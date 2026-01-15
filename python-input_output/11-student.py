10ff7ce440e70b476b301676c711e734d9a68d62
lebomofube1818-sys <lebomofube1818-sys@gmail.com>
2026-01-15 16:36:53 +0200

    Task 11: Student class with JSON save/load methods

---
+++ python-input_output/11-student.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Module that defines a Student class with JSON serialization/deserialization."""
+
+
+class Student:
+    """Student class with first_name, last_name, and age."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance.
+
+        Args:
+            first_name (str): First name of the student.
+            last_name (str): Last name of the student.
+            age (int): Age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieve a dictionary representation of the Student instance.
+
+        If attrs is a list of strings, only attributes in this list are returned.
+        Otherwise, all attributes are returned.
+
+        Args:
+            attrs (list, optional): List of attribute names to retrieve.
+
+        Returns:
+            dict: Dictionary representation of the student.
+        """
+        if attrs is None:
+            return self.__dict__.copy()
+        if isinstance(attrs, list):
+            filtered_dict = {}
+            for attr in attrs:
+                if attr in self.__dict__:
+                    # Keep within 79 characters
+                    filtered_dict[attr] = self.__dict__.get(attr)
+            return filtered_dict
+        return self.__dict__.copy()
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student instance.
+
+        Args:
+            json (dict): Dictionary containing new attributes and values.
+        """
+        if isinstance(json, dict):
+            for key, value in json.items():
+                setattr(self, key, value)
