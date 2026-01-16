13a01ffbed7f7767e600331b9ba08bcb83b47eb7
lebomofube1818-sys <lebomofube1818-sys@gmail.com>
2026-01-16 12:09:48 +0200

    Task 3: Infinite addition

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Adds all command-line arguments.
+"""
+import sys
+
+
+def infinite_add():
+    total = 0
+
+    for arg in sys.argv[1:]:
+        total += int(arg)
+
+    print(total)
+
+
+if __name__ == "__main__":
+    infinite_add()
