d6f974b4730e7c083ac1ef6e2b49854d8ff95d93
lebomofube1818-sys <lebomofube1818-sys@gmail.com>
2026-01-23 17:09:23 +0200

    Add print_square function with doctests

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This module provides a function that prints a square using '#'.
+"""
+
+
+def print_square(size):
+    """
+    Prints a square with the character '#'.
+
+    Args:
+        size (int): The size of the square.
+
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is less than 0.
+    """
+    if isinstance(size, float):
+        raise TypeError("size must be an integer")
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for _ in range(size):
+        print("#" * size)
