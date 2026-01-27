f3829105da76e220e8ebf086a92f7fef8965efee
Realeboha <lebomofube1818@gmail.com>
2026-01-27 11:01:39 +0000

    Add 0-list_databases.sql: alias SCHEMA_NAME to Database

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,6 +1,6 @@
 -- 0. List all databases of your MySQL server
 -- This script lists all databases currently present, sorted alphabetically
-SELECT SCHEMA_NAME
+SELECT SCHEMA_NAME AS `Database`
 FROM INFORMATION_SCHEMA.SCHEMATA
 ORDER BY SCHEMA_NAME ASC;
 
