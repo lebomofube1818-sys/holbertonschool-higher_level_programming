78ff1078da8e47539596ec8d4493dfbf6f9a536f
Realeboha <lebomofube1818-sys>
2026-01-29 13:01:34 +0000

    List all cities with state names using JOIN, sorted by cities.id

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- lists all cities with their state names
+SELECT cities.id, cities.name, states.name AS state_name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
+
