b1fec16e689a1653a3042568e28cc281b8fe8479
Realeboha <lebomofube1818-sys>
2026-01-29 13:07:53 +0000

    List all cities with state names using JOIN, sorted by cities.id

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
--- lists all cities with their state names
+-- 9. List all cities with their state names
 SELECT cities.id, cities.name, states.name AS state_name
 FROM cities
 JOIN states ON cities.state_id = states.id
