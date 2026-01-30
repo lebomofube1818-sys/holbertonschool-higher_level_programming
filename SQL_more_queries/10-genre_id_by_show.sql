bc9c054070d221baf58776b14f9e0c628c531dce
Realeboha <lebomofube1818@gmail.com>
2026-01-30 12:01:13 +0000

    Task 10: Query for genre_id by show and database dump

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,12 @@
+-- 10. Genre ID by show
+-- List all shows that have at least one genre linked
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- Results sorted by tv_shows.title and tv_show_genres.genre_id
+-- You can use only one SELECT statement
+
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
+  ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+
